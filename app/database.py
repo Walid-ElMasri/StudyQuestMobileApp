b51d7ff060c62589c5cdf6253ae687031fb55f22
walidmasri <walidalmasri2004@gmail.com>
2025-10-18 20:50:17 +0300

    dbkey fix

--- app/database.py
+++ app/database.py
@@ -7,8 +7,12 @@ def _build_engine():
     database_url = os.getenv("DATABASE_URL")
 
     if database_url:
+        if database_url.startswith("postgresqlpsycopg://"):
+            database_url = database_url.replace("postgresqlpsycopg://", "postgresql+psycopg://", 1)
         if database_url.startswith("postgres://"):
             database_url = database_url.replace("postgres://", "postgresql+psycopg://", 1)
+        if database_url.startswith("postgresql://"):
+            database_url = database_url.replace("postgresql://", "postgresql+psycopg://", 1)
         return create_engine(database_url, echo=False)
 
     sqlite_file_name = "studyquest.db"
